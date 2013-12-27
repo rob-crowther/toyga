fdead4fa1329d5b408b59548f9622b05f7560aed
weilawei <weilawei@gmail.com>
2013-12-27 18:14:13 -0500

    savegame

--- amp.py
+++ amp.py
@@ -10,6 +10,8 @@ import math
 import time
 from pprint import pprint as pp
 
+debug = True
+
 the_weights = [
     -1.3,   #   Maximum attenuation in the pass band
      1.0,   #   Minimum attenuation in the stop band
@@ -20,14 +22,13 @@ the_weights = [
 ]
 
 class Component(object):
-    common  = list()
-    value   = None
-    name    = None
+    common      = list()
+    value       = None
+    part_id     = None
+    ext_n1      = None
+    ext_n2      = None
 
-    def __init__(self, value=None):
-        self.value = value if value else random.choice(self.common)
-
-def Resistor(Component):
+class Resistor(Component):
     common = [  #   In ohms
         10, 11, 12, 13, 15, 16, 18, 20, 22, 24, 27, 30, 33, 36, 39, 43, 47, 51, 56, 62, 68, 75, 82,
         91, 100, 110, 120, 130, 150, 160, 180, 200, 220, 240, 270, 300, 330, 360, 390, 430, 470, 
@@ -44,7 +45,7 @@ def Resistor(Component):
         36000000, 39000000, 43000000, 47000000, 51000000, 56000000, 62000000, 68000000, 75000000, 
         82000000, 91000000]
 
-def Inductor(Component):
+class Inductor(Component):
     common = [  #   In henries
         1e-09, 1e-08, 1e-07, 1e-06, 1e-05, 1e-09, 1.1e-08, 1.1e-07, 1.1e-06, 1.1e-05, 1e-09, 
         1.2e-08, 1.2e-07, 1.2e-06, 1.2e-05, 1e-09, 1.3e-08, 1.3e-07, 1.3e-06, 1.3e-05, 2e-09, 
@@ -72,80 +73,71 @@ class Capacitor(Component):
         8.2e-06]
 
 class Circuit(object):
-    parameters  = None
-    result      = None
-    circuit     = None
-    resistors   = None
-    next_node   = 1
+    passives        = None
+    num_r           = None
+    num_l           = None
+    num_c           = None
+    num_parts       = None
+    next_part_id    = None
+    num_nodes       = None
+    all_nodes       = None
+    circuit         = None
+    result          = None
 
-    def __init__(self, parameters=None):
-        self.parameters             = dict()
-        self.parameters['num_R']    = random.randint(1, 4)
-        self.parameters['num_L']    = random.randint(1, 4)
-        self.parameters['num_C']    = random.randint(1, 4)
-        self.parameters['nodes']    = sum([
-            self.parameters['num_R'],
-            self.parameters['num_L'],
-            self.parameters['num_C']])
-
-        self.parameters['passives'] = list()
+    def __init__(self, num_r=None, num_l=None, num_c=None, num_parts=None, num_nodes=None, passives=None, next_part_id=None, next_node_id=None):
+        self.num_r          = num_r if num_r else random.randint(1, 4)
+        self.num_l          = num_l if num_l else random.randint(1, 4)
+        self.num_c          = num_c if num_c else random.randint(1, 4)
+        self.num_parts      = num_parts if num_parts else sum([self.num_r, self.num_l, self.num_c])
+        self.num_nodes      = num_nodes if num_nodes else sum([self.num_r, self.num_l, self.num_c])
+        self.passives       = passives if passives else list()
+        self.next_part_id   = next_part_id if next_part_id else 0
+        self.next_node_id   = next_node_id if next_node_id else 0
 
         #   Create a list of all the nodes to select from, emphasizing ground
-        self.all_nodes = (['0'] * 3) + ["n%d" % i for i in range(1, self.parameters['nodes'])]
+        self.all_nodes = (['0'] * 3) + ["n%d" % i for i in range(1, self.num_nodes)]
 
-        for i in range(0, self.parameters['num_R']):
-            a_resistor              = dict()
-            a_resistor['name']      = "R%d" % random.randint(1, self.parameters['num_R'])
-            a_resistor['ext_n1']    = random.choice(self.all_nodes)
-            a_resistor['ext_n2']    = random.choice(self.all_nodes)
-            a_resistor['value']     = random.choice(self.resistors)
+        #   Create components
+        for component_type, component_class in [('R', Resistor), ('L', Inductor), ('C', Capacitor)]:
+            num_component = 0
+            if component_type   == 'R': num_component = self.num_r
+            elif component_type == 'L': num_component = self.num_l
+            elif component_type == 'C': num_component = self.num_c
 
-            self.parameters['passives'].append(a_resistor)
-
-        for i in range(0, self.parameters['num_L']):
-            an_inductor             = dict()
-            an_inductor['name']     = "L%d" % random.randint(1, self.parameters['num_L'])
-            an_inductor['ext_n1']   = random.choice(self.all_nodes)
-            an_inductor['ext_n2']   = random.choice(self.all_nodes)
-            an_inductor['value']    = random.randint(0, 1000)/1000000.0
-
-            self.parameters['passives'].append(an_inductor)
-
-        for i in range(0, self.parameters['num_C']):
-            a_capacitor             = dict()
-            a_capacitor['name']     = "C%d" % random.randint(1, self.parameters['num_C'])
-            a_capacitor['ext_n1']   = random.choice(self.all_nodes)
-            a_capacitor['ext_n2']   = random.choice(self.all_nodes)
-            a_capacitor['value']    = random.randint(0, 1000)/1000000000.0
-
-            self.parameters['passives'].append(a_capacitor)
+            for i in range(0, num_component):
+                a_part              = component_class()
+                a_part.value        = random.choice(a_part.common)
+                a_part.part_id      = "%s%d" % (component_type, self.next_part_id)
+                a_part.ext_n1       = random.choice(self.all_nodes)
+                a_part.ext_n2       = random.choice(self.all_nodes)
+                self.passives.append(a_part)
+                self.next_part_id += 1
 
     #   Random correlation between parameters and simulation scores
     def simulate(self):
         if self.result: return self.result
 
-        try:
-            self.circuit = circuit.circuit(title=str(self.parameters))
+        self.circuit = circuit.circuit(title='Untitled')
 
-            for i in range(0, self.parameters['nodes']):
-                self.circuit.create_node("n%d" % self.next_node)
-                self.next_node += 1
-            
-            for i in self.parameters['passives']:    
-                if (i['name'][0] == 'R'):
-                    self.circuit.add_resistor(i['name'], i['ext_n1'],  i['ext_n2'], R=i['value'])
-                elif (i['name'][0] == 'L'):
-                    self.circuit.add_inductor(i['name'], i['ext_n1'],  i['ext_n2'], L=i['value'])
-                elif (i['name'][0] == 'C'):
-                    self.circuit.add_capacitor(i['name'], i['ext_n1'],  i['ext_n2'], C=i['value'])
+        #   Create nodes
+        for i in range(0, self.num_nodes):
+            self.circuit.create_node("n%d" % self.next_node_id)
+            self.next_node_id += 1
+        
+        #   Create passive components
+        for i in self.passives:
+            if (i.part_id[0] == 'R'):
+                self.circuit.add_resistor(i.part_id, i.ext_n1,  i.ext_n2, R=i.value)
+            elif (i.part_id[0] == 'L'):
+                self.circuit.add_inductor(i.part_id, i.ext_n1,  i.ext_n2, L=i.value)
+            elif (i.part_id[0] == 'C'):
+                self.circuit.add_capacitor(i.part_id, i.ext_n1,  i.ext_n2, C=i.value)
 
-            voltage_step = devices.pulse(v1=0, v2=1, td=500e-9, tr=1e-12, pw=1, tf=1e-12, per=2)
-            self.circuit.add_vsource(name="V1", ext_n1='n1', ext_n2='0', vdc=5, vac=1, function=voltage_step)
+        voltage_step = devices.pulse(v1=0, v2=1, td=500e-9, tr=1e-12, pw=1, tf=1e-12, per=2)
+        self.circuit.add_vsource(name="V1", ext_n1='n1', ext_n2='0', vdc=5, vac=1, function=voltage_step)
 
-            ac_analysis = ahkab.new_ac(start=1e3, stop=1e5, points=100)
-            self.result = ahkab.run(self.circuit, an_list=[ac_analysis])
-        except:
-            return None
+        ac_analysis = ahkab.new_ac(start=1e3, stop=1e5, points=100)
+        self.result = ahkab.run(self.circuit, an_list=[ac_analysis])
 
         return self.result
 
@@ -153,8 +145,8 @@ class Circuit(object):
         print "\n\n"
         r = self.simulate()
 
-        if not r:
-            return -9000
+        #   Didn't simulate
+        if not r: return None
         
         try:
             # Normalize the output to the low frequency value and convert to array
@@ -178,35 +170,35 @@ class Circuit(object):
             max_attenuation_pass_band = (2e3, -1.0*norm_out_db_interpolated(2e3))
             min_attenuation_stop_band = (6.5e3, -1.0*norm_out_db_interpolated(6.5e3))
             
-            print "Maximum attenuation in the pass band (0-%g Hz) is %g dB" % max_attenuation_pass_band
-            print "Minimum attenuation in the stop band (%g Hz - Inf) is %g dB" % min_attenuation_stop_band
-            
             #   Eliminate unusable results
-            if (-1.0*norm_out_db_interpolated(2e3)) == -0:
-                return -9000
-            if (-1.0*norm_out_db_interpolated(6.5e3)) == -0:
-                return -9000
+            if (max_attenuation_pass_band[1] == -0) or math.isnan(max_attenuation_pass_band[1]):
+                return None
+            if (min_attenuation_stop_band[1] == -0) or math.isnan(min_attenuation_stop_band[1]):
+                return None
+
+            if debug:
+                printing.print_circuit(self.circuit)
+                print "Maximum attenuation in the pass band (0-%g Hz) is %g dB" % max_attenuation_pass_band
+                print "Minimum attenuation in the stop band (%g Hz - Inf) is %g dB" % min_attenuation_stop_band
 
             #   Form a draft of the final score
             a_score = [
                 max_attenuation_pass_band[1], 
                 min_attenuation_stop_band[1], 
-                self.parameters['nodes'],
-                self.parameters['num_R'],
-                self.parameters['num_L'],
-                self.parameters['num_C'],
-            ]
+                self.num_nodes,
+                self.num_r,
+                self.num_l,
+                self.num_c]
 
             #   Weight the draft versions of the final scores
             return sum([a_weight * a_score for (a_weight, a_score) in zip(the_weights, a_score)])
         
         #   If ANYTHING goes wrong, we just mark the circuit as bad. Lazy. (Possibly bad. Probably bad.)
-        except:
-            return -9000
+        except: return None
 
 if __name__ == "__main__":
     desired_score   = 20
-    population_size = 10
+    population_size = 100
     the_population  = [Circuit() for i in range(0, population_size)]
     the_generation  = 0
 
@@ -216,16 +208,16 @@ if __name__ == "__main__":
 
         #   Simulate and score each member of the population
         for a_member in the_population:
-            the_scores.append((a_member.score(), a_member))
+            a_score = a_member.score()
+            if a_score: the_scores.append((a_score, a_member))
 
-        #   Remove unusable results and sort the remainder
-        the_scores = filter(lambda x: not math.isnan(x[0]), the_scores)
-        the_scores = filter(lambda x: x[0] != -9000, the_scores)
+        #   Sort the results
         the_scores.sort(key=lambda x: -x[0])
 
         #   Print out the remaining circuits
         print "\n\n"
-        pp(the_scores)
+        if the_scores:
+            pp(the_scores)
         print "\n"
 
         try:
@@ -248,7 +240,7 @@ if __name__ == "__main__":
 
         #   Keep up to the first 3 and generate up to 97 new ones
         the_population =  [a_score[1] for a_score in the_scores[:3]]
-        the_population += [Circuit() for i in range(0, 100 - len(the_population))]
+        the_population += [Circuit() for i in range(0, population_size - len(the_population))]
 
         print "\n"
 

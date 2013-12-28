e857ada867de02e5e557753eca25278012f6a4e4
weilawei <weilawei@gmail.com>
2013-12-28 09:03:37 -0500

    savegame

--- amp.py
+++ amp.py
@@ -3,6 +3,7 @@
 import random
 import ahkab
 from ahkab import circuit, printing, devices
+import tempfile
 import scipy
 import scipy.interpolate
 import numpy
@@ -13,7 +14,6 @@ from pprint import pprint as pp
 debug = False
 
 class Component(object):
-    common      = list()
     value       = None
     part_id     = None
     ext_n1      = None
@@ -70,11 +70,10 @@ class Circuit(object):
     num_c                       = None  #   Number of capacitors
     num_parts                   = None  #   Number of passive parts in total
     next_part_id                = None  #   Next available part id
-    next_node_id                = None  #   Next available node id
     num_nodes                   = None  #   Number of connection nodes
     all_nodes                   = None  #   A list of every node
-    circuit                     = None  #   An `akhab` circuit object
-    result                      = None  #   `akhab` simulation results dict
+    circuit                     = None  #   An Ahkab circuit object
+    outfile                     = None  #   The filename for Ahkab's scratchpad
     max_attenuation_pass_band   = None  #   Tuple of (pass band upper frequency, maximum attenuation)
     min_attenuation_stop_band   = None  #   Tuple of (stop band lower frequency, minimum attenuation)
     weights                     = [
@@ -93,8 +92,8 @@ class Circuit(object):
         num_parts=None,     
         num_nodes=None,     
         passives=None,      
-        next_part_id=None,  
-        next_node_id=None,
+        next_part_id=None,
+        outfile=None,  
         weights=None): 
 
         #   Assign provided values or random defaults
@@ -105,7 +104,7 @@ class Circuit(object):
         self.num_nodes              = num_nodes if num_nodes else sum([self.num_r, self.num_l, self.num_c])
         self.passives               = passives if passives else list()
         self.next_part_id           = next_part_id if next_part_id else 0
-        self.next_node_id           = next_node_id if next_node_id else 0
+        self.outfile                = outfile if outfile else 'ramdisk/sim.ac'
         if weights: self.weights    = weights
 
         #   Create a list of all the nodes to select from, emphasizing ground
@@ -130,14 +129,16 @@ class Circuit(object):
                 self.passives.append(a_part)
                 self.next_part_id += 1
 
-    #   Drive the `ahkab` circuit simulator
+    #   Drive the Ahkab circuit simulator
     def simulate(self):
         self.circuit = circuit.circuit(title='Untitled')
 
+        next_node_id = 0
+
         #   Create nodes
         for i in range(0, self.num_nodes):
-            self.circuit.create_node("n%d" % self.next_node_id)
-            self.next_node_id += 1
+            self.circuit.create_node("n%d" % next_node_id)
+            next_node_id += 1
         
         #   Create passive components
         for i in self.passives:
@@ -153,10 +154,7 @@ class Circuit(object):
         self.circuit.add_vsource(name="V1", ext_n1='n1', ext_n2='0', vdc=5, vac=1, function=voltage_step)
 
         #   Simulate the circuit with an AC analysis
-        ac_analysis = ahkab.new_ac(start=1e3, stop=1e5, points=100)
-        self.result = ahkab.run(self.circuit, an_list=[ac_analysis])
-
-        return self.result
+        return ahkab.ac.ac_analysis(self.circuit, 1e3, 100, 1e5, 'LOG', outfile=self.outfile, verbose=0)
 
     def score(self):
         r = self.simulate()
@@ -168,7 +166,7 @@ class Circuit(object):
         
         try:
             # Normalize the output to the low frequency value and convert to array
-            norm_out = numpy.asarray(r['ac']['|Vn4|'].T/r['ac']['|Vn4|'].max())
+            norm_out = numpy.asarray(r['|Vn4|'].T/r['|Vn4|'].max())
             
             # Convert to dB
             norm_out_db = 20 * numpy.log10(norm_out)
@@ -177,7 +175,7 @@ class Circuit(object):
             norm_out_db = norm_out_db.reshape((max(norm_out_db.shape),))
             
             # Convert angular frequencies to Hz and convert matrix to array
-            frequencies = numpy.asarray(r['ac']['w'].T/2/math.pi)
+            frequencies = numpy.asarray(r['w'].T/2/math.pi)
             
             # Reshape to be scipy-friendly
             frequencies = frequencies.reshape((max(frequencies.shape),))
@@ -196,8 +194,8 @@ class Circuit(object):
 
             if debug:
                 printing.print_circuit(self.circuit)
-                print "Maximum attenuation in the pass band (0-%g Hz) is %g dB" % max_attenuation_pass_band
-                print "Minimum attenuation in the stop band (%g Hz - Inf) is %g dB" % min_attenuation_stop_band
+                print "Maximum attenuation in the pass band (0-%g Hz) is %g dB" % self.max_attenuation_pass_band
+                print "Minimum attenuation in the stop band (%g Hz - Inf) is %g dB" % self.min_attenuation_stop_band
 
             #   Form a draft of the final score
             a_score = [
@@ -208,66 +206,85 @@ class Circuit(object):
                 self.num_l,
                 self.num_c]
 
-            #   Weight the draft versions of the final scores
+            #   Weight the draft version of the final score
             return sum([a_weight * a_score for (a_weight, a_score) in zip(self.weights, a_score)])
         
         #   If ANYTHING goes wrong, we just mark the circuit as bad. Lazy. (Possibly bad. Probably bad.)
         except: return None
 
+class Population(list):
+    population_size = None
+    top_n           = None
+    generation      = None
+
+    def __init__(self, population=None, population_size=None, top_n=None, generation=None):
+        self.population_size    = population_size if population_size    else 50
+        self.top_n              = top_n if top_n                        else 3
+        self.generation         = generation if generation              else 0
+
+        self.repopulate(population=population, population_size=self.population_size)
+
+    def repopulate(self, population=None, population_size=None):
+        self.__delslice__(0, self.population_size)
+        if population_size: self.population_size = population_size
+        if population:
+            self += population
+        else:
+            self += [Circuit() for i in range(0, self.population_size)]
+
+    def simulate(self):
+        while True:
+            scores = []
+
+            #   Simulate and score each member of the population
+            for a_member in self:
+                a_score = a_member.score()
+                if a_score: scores.append((a_score, a_member))
+
+            #   Sort the results
+            scores.sort(key=lambda x: -x[0])
+
+            if not scores:
+                self.repopulate()
+                print "Generation %d: mulligan" % self.generation
+                self.generation += 1
+
+                #   Pause for humans to read post-generation results (Optional)
+                time.sleep(1)
+                continue
+
+            #   Return a tuple of (last simulated generation, [(score, circuit)...])
+            yield (self.generation, scores)
+            self.generation += 1
+
+            #   Keep up to the first `top_n` and generate up to (`population_size` - `top_n`) new ones
+            new_population =  [a_score[1] for a_score in scores[:self.top_n]]
+            new_population += [Circuit() for i in range(0, self.population_size - len(new_population))]
+            self.__delslice__(0, self.population_size)
+            self += new_population
+
 if __name__ == "__main__":
-    desired_score   = 20
-    population_size = 10
-    top_n           = 3
-    the_population  = [Circuit() for i in range(0, population_size)]
-    the_generation  = 0
+    desired_score   = 5
+    top_score       = None
+    a_population    = Population()
 
-    #   Loop until greater than `desired_score` 
-    while True:
-        the_scores = []
-
-        #   Simulate and score each member of the population
-        for a_member in the_population:
-            a_score = a_member.score()
-            if a_score: the_scores.append((a_score, a_member))
-
-        #   Sort the results
-        the_scores.sort(key=lambda x: -x[0])
-
+    for (generation, scores) in a_population.simulate() :
         #   Print out the remaining circuits
         print "\n\n"
-        if the_scores:
-            print "Scores\n------\n"
-            pp(the_scores)
-        print "\n"
-
-        try:
-            top_score = the_scores[0]
-
-        #   Take a mulligan
-        except IndexError:
-            print "Generation %d: mulligan" % the_generation
-
-            #   Pause for humans to read post-generation results (Optional)
-            time.sleep(1)
-
-            the_generation += 1;
-            the_population = [Circuit() for i in range(0, population_size)]
-            continue
+        top_score = scores[0]
+        print "Scores\n------\n"
+        pp(scores)
+        print "\n"        
 
         #   Print the top score
-        print "Top Score (generation %d): score: %s\n\n" % (the_generation, top_score[0])
+        print "Top Score (generation %d): score: %s\n\n" % (generation, top_score[0])
         printing.print_circuit(top_score[1].circuit)
         print "\nMaximum attenuation in the pass band (0-%g Hz) is %g dB" % top_score[1].max_attenuation_pass_band
         print "Minimum attenuation in the stop band (%g Hz - Inf) is %g dB" % top_score[1].min_attenuation_stop_band
-        the_generation += 1
 
         #   Good Enough answer found
         if top_score[0] > desired_score:
             break
 
         #   Pause for humans to read post-generation results (Optional)
-        time.sleep(5)
-
-        #   Keep up to the first `top_n` and generate up to (`population_size` - `top_n`) new ones
-        the_population =  [a_score[1] for a_score in the_scores[:top_n]]
-        the_population += [Circuit() for i in range(0, population_size - len(the_population))]
+        time.sleep(2)
